969e08b7953616694fefdefd8ddb8a9c90ce2bdc
funkycram <contact@marc.fun>
2022-05-08 17:41:21 +0200

    update

---
+++ login/resources/js/transition-network-v.1.1.js
@@ -0,0 +1,4 @@
+$(function(){
+	$('#kc-info.login-pf-signup').detach().prependTo('#kc-content-wrapper');
+	$('#kc-page-title').text('Welcome! Please register if you don\'t have an account yet.');
+});
